8e44e1e5373fd379c71e117bb5c528dd778b1234
Harinimatheswaran <151748095+Harinimatheswaran@users.noreply.github.com>
2025-01-07 08:52:29 +0530

    message

--- src/Apps.jsx
+++ src/Apps.jsx
@@ -1,24 +1,4 @@
-/*import React from 'react';
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
-import Createuser from './Createuser';
-import Updateuser from './Updateuser';
-import User from './User';
-//import "./App.css";
 
-const Apps = () => {
-    return(
-        <div>
-            <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<User />} />
-                <Route path="/create" element={<Createuser />} />
-                <Route path="/update" element={<Updateuser />} />
-            </Routes>
-            </BrowserRouter>
-        </div>
-    )
-}
-export default Apps*/
 import React from "react";
 
 import { BrowserRouter as Router , Routes , Route } from "react-router-dom";
@@ -36,7 +16,8 @@ const Apps = () => {
                 <Routes>
                     <Route path="/" element={<User/>} />
                     <Route path="/create" element={<Createuser/>} />  
-                    <Route path="/update" element={<Updateuser/>} />
+                    <Route path="/update/:id" element={<Updateuser/>} />
+                
                 </Routes>
             </Router>    
         </div>
@@ -44,3 +25,4 @@ const Apps = () => {
 };
 
 export default Apps;
+
